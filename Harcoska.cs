3299d78b8011abeaab7a4f062c389aa6a1a058f0
gillichnorbert <106962782+gillichnorbert@users.noreply.github.com>
2024-02-19 00:35:36 +0100

    Add files via upload

---
+++ Harcoska.cs
@@ -0,0 +1,56 @@
+﻿using System;
+namespace Harcosos
+{
+	public class Harcos
+	{
+        private string nev;
+        private int eletEro;
+		private int harciEro;
+
+        public string Nev
+        {
+            get { return nev; }
+            set { nev = value; }
+        }
+
+        public int Eletero
+        {
+            get { return eletEro; }
+            set { eletEro = value; }
+        }
+
+        public int HarciEro
+        {
+            get { return harciEro; }
+            set { harciEro = value; }
+        }
+
+        public Harcos(string nev, int harciEro, int eletEro)
+		{
+            this.nev = nev;
+            this.eletEro = eletEro;
+            this.harciEro = harciEro;
+        }
+        public bool Harcol(Harcos masikHarcos)
+        {
+            masikHarcos.eletEro -= this.harciEro;
+            this.eletEro -= masikHarcos.HarciEro;
+
+            if (this.eletEro <= 0 || masikHarcos.eletEro <= 0)
+                return true;
+            else
+                return false;
+        }
+
+        public override string? ToString()
+        {
+            return $"Név: {nev}, Életerő: {eletEro}, Harci erő: {harciEro}";
+        }
+
+
+
+
+    }
+
+}
+
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using Harcosos;
+
+Harcos harcos1 = new Harcos("Kálmán", 15000, 20);
+Harcos harcos2 = new Harcos("Alekosz", 2000, 50);
+
+Console.WriteLine("A harcosok harcba szállnak:");
+Console.WriteLine(harcos1);
+Console.WriteLine(harcos2);
+Console.WriteLine();
+
+while (!harcos1.Harcol(harcos2))
+{
+    Console.WriteLine($"{harcos1.Nev} életereje: {harcos1.Eletero}, {harcos2.Nev} életereje: {harcos2.Eletero}");
+}
+
+if (harcos1.Eletero <= 0)
+    Console.WriteLine($"{harcos1.Nev} vesztett!");
+else
+    Console.WriteLine($"{harcos2.Nev} vesztett!");
+
+Console.ReadKey();
